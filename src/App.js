65fb41efb3f708dda1a82eda1dc6c1e5ddea6a27
Leanna <leanna.resurreccion@gmail.com>
2020-06-02 00:01:16 -0700

    finished basics of mobile site

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Components/Header'
 import Intro from './Components/Intro'
 import Projects from './Components/Projects'
+import Contact from './Components/Contact'
 import './Styles/main.css';
 import './App.css';
 import data from './Components/data'
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Intro heading={data.introHead} body={data.introBody} />
       <Projects />
+      <Contact />
     </div>
   );
 }
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import Github from '../Icons/Github'
+import Linkedin from '../Icons/Linkedin'
 
 export default function Contact() {
     return (
         <div className="Contact">
-            
+            <p className="Contact__header">Interested in working together? Let's talk!</p>
+            <a href="mailto:leanna.resurreccion@gmail.com" className="Contact__button">Email</a>
+            <div className="Contact_social">
+                <a href="github.com/resurreccionl"><Github fill="#65507E" /></a>
+                <a href="https://www.linkedin.com/in/leanna-resurreccion-b3b54a130/"><Linkedin fill="#65507E"/></a>
+            </div>
         </div>
     )
 }
